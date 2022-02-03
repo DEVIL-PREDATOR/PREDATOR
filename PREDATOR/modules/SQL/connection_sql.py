3e0ab00fcdcc2db7067cc3b2492a4603bad472bc
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-03 23:26:20 +0530

    Update connection_sql.py

--- PREDATOR/modules/SQL/connection_sql.py
+++ PREDATOR/modules/SQL/connection_sql.py
@@ -5,7 +5,7 @@ from typing import Union
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 from sqlalchemy.sql.sqltypes import BigInteger
 
-from Yuriko.modules.sql import SESSION, BASE
+from Predator.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
