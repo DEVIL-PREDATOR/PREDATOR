fef8880d986c32672dec719d7daf7b23c47e12f4
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-03 23:34:48 +0530

    Update kuki_sql.py

--- PREDATOR/modules/SQL/kuki_sql.py
+++ PREDATOR/modules/SQL/kuki_sql.py
@@ -1,42 +1,42 @@
 import threading
 from sqlalchemy import Column, String
-from Yuriko.modules.sql import BASE, SESSION
+from Predator.modules.sql import BASE, SESSION
 class KukiChats(BASE):
-    __tablename__ = "kuki_chats"
+    __tablename__ = "knight_chats"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
-KukiChats.__table__.create(checkfirst=True)
+knightChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_kuki(chat_id):
+def is_knight(chat_id):
     try:
-        chat = SESSION.query(KukiChats).get(str(chat_id))
+        chat = SESSION.query(knightChats).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
-def set_kuki(chat_id):
+def set_knight(chat_id):
     with INSERTION_LOCK:
-        kukichat = SESSION.query(KukiChats).get(str(chat_id))
-        if not kukichat:
-            kukichat = KukiChats(str(chat_id))
-        SESSION.add(kukichat)
+        kukichat = SESSION.query(knightChats).get(str(chat_id))
+        if not knightchat:
+           knightchat = knightChats(str(chat_id))
+        SESSION.add(knightchat)
         SESSION.commit()
 
-def rem_kuki(chat_id):
+def rem_knight(chat_id):
     with INSERTION_LOCK:
-        kukichat = SESSION.query(KukiChats).get(str(chat_id))
+        kukichat = SESSION.query(knightChats).get(str(chat_id))
         if kukichat:
-            SESSION.delete(kukichat)
+            SESSION.delete(knightchat)
         SESSION.commit()
 
 
 def get_all_kuki_chats():
     try:
-        return SESSION.query(KukiChats.chat_id).all()
+        return SESSION.query(knightChats.chat_id).all()
     finally:
         SESSION.close()
