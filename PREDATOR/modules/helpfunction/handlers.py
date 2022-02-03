844e6c6457299dc3d67f9d06c02ea3076a5330f7
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-03 23:00:26 +0530

    Update handlers.py

--- PREDATOR/modules/helpfunction/handlers.py
+++ PREDATOR/modules/helpfunction/handlers.py
@@ -1,6 +1,6 @@
-import Yuriko.modules.sql.blacklistusers_sql as sql
-from Yuriko import ALLOW_EXCL
-from Yuriko import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import Predator.modules.sql.blacklistusers_sql as sql
+from Predator import ALLOW_EXCL
+from Predator import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
