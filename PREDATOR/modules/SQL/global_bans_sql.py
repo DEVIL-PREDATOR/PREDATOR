76a0344e9d28e039b0a7d73b2f50311bb025c255
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-03 23:31:49 +0530

    Update global_bans_sql.py

--- PREDATOR/modules/SQL/global_bans_sql.py
+++ PREDATOR/modules/SQL/global_bans_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy.sql.sqltypes import BigInteger
-from Yuriko.modules.sql import BASE, SESSION
+from Predator.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, String, UnicodeText
 
 
