b1aaf6c4b3469512aa5f76da9da730dc10ac1c77
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-03 23:40:32 +0530

    Update nsfw_sql.py

--- PREDATOR/modules/SQL/nsfw_sql.py
+++ PREDATOR/modules/SQL/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from Yuriko.modules.sql import BASE, SESSION
+from Predator.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
