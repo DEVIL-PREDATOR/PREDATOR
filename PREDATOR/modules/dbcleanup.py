056b1300b39f1e738213f69b1ee26fa968b30a02
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-04 23:30:15 +0530

    Update dbcleanup.py

--- PREDATOR/modules/dbcleanup.py
+++ PREDATOR/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import Yuriko.modules.sql.global_bans_sql as gban_sql
-import Yuriko.modules.sql.users_sql as user_sql
-from Yuriko import DEV_USERS, OWNER_ID, dispatcher
-from Yuriko.modules.helper_funcs.chat_status import dev_plus
+import Predator.modules.sql.global_bans_sql as gban_sql
+import Predator.modules.sql.users_sql as user_sql
+from Predator import DEV_USERS, OWNER_ID, dispatcher
+from Predator.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
