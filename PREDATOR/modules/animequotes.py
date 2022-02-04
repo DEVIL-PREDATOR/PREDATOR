a98522d5b04b3b176fb98925456fe196941fef94
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-04 22:31:41 +0530

    Update animequotes.py

--- PREDATOR/modules/animequotes.py
+++ PREDATOR/modules/animequotes.py
@@ -4,9 +4,9 @@ import html
 import random
 import time
 
-from Yuriko import dispatcher
-from Yuriko.modules.disable import DisableAbleCommandHandler
-from Yuriko.modules.helper_funcs.chat_status import (is_user_admin)
+from Predator import dispatcher
+from Predator.modules.disable import DisableAbleCommandHandler
+from Predator.modules.helper_funcs.chat_status import (is_user_admin)
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async, CallbackQueryHandler
 from telegram import ParseMode, Update, InlineKeyboardMarkup, InlineKeyboardButton, replymarkup, ChatPermissions
 from telegram.error import BadRequest
