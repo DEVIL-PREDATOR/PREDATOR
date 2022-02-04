c0018594828fe5482fb0d38cdf55bac5bcd54bd6
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-04 22:58:10 +0530

    Update blacklist.py

--- PREDATOR/modules/blacklist.py
+++ PREDATOR/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import Yuriko.modules.sql.blacklist_sql as sql
-from Yuriko import dispatcher, LOGGER
-from Yuriko.modules.disable import DisableAbleCommandHandler
-from Yuriko.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from Yuriko.modules.helper_funcs.extraction import extract_text
-from Yuriko.modules.helper_funcs.misc import split_message
-from Yuriko.modules.log_channel import loggable
-from Yuriko.modules.warns import warn
-from Yuriko.modules.helper_funcs.string_handling import extract_time
-from Yuriko.modules.connection import connected
-from Yuriko.modules.sql.approve_sql import is_approved
-from Yuriko.modules.helper_funcs.alternate import send_message, typing_action
+import Predator.modules.sql.blacklist_sql as sql
+from Predator import dispatcher, LOGGER
+from Predator.modules.disable import DisableAbleCommandHandler
+from Predator.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Predator.modules.helper_funcs.extraction import extract_text
+from Predator.modules.helper_funcs.misc import split_message
+from Predator.modules.log_channel import loggable
+from Predator.modules.warns import warn
+from Predator.modules.helper_funcs.string_handling import extract_time
+from Predator.modules.connection import connected
+from Predator.modules.sql.approve_sql import is_approved
+from Predator.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
