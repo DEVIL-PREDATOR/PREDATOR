d9432144dcf16d0e46ad8f1269b889a6ad345a37
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-05 20:35:59 +0530

    Update fun.py

--- PREDATOR/modules/fun.py
+++ PREDATOR/modules/fun.py
@@ -2,11 +2,11 @@ import html
 import random
 import time
 
-import Yuriko.modules.fun_strings as fun_strings
-from Yuriko import dispatcher
-from Yuriko.modules.disable import DisableAbleCommandHandler
-from Yuriko.modules.helper_funcs.chat_status import is_user_admin
-from Yuriko.modules.helper_funcs.extraction import extract_user
+import Predator.modules.fun_strings as fun_strings
+from Predator import dispatcher
+from Predator.modules.disable import DisableAbleCommandHandler
+from Predator.modules.helper_funcs.chat_status import is_user_admin
+from Predator.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext
