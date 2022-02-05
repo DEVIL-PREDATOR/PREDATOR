b43fba574f63b42533f5410d7901db9726079f8b
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-05 21:28:17 +0530

    Update json.py

--- PREDATOR/modules/json.py
+++ PREDATOR/modules/json.py
@@ -1,6 +1,6 @@
 import io
-from Yuriko.events import register
-from Yuriko import telethn as tbot
+from Predator.events import register
+from Predator import telethn as tbot
 from telethon import types
 from telethon import events
 from telethon.tl import functions
