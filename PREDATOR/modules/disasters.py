724c53cc89261bea1a3bcb7df68d76c26d9856e4
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-05 20:25:29 +0530

    Update disasters.py

--- PREDATOR/modules/disasters.py
+++ PREDATOR/modules/disasters.py
@@ -3,7 +3,7 @@ import json
 import os
 from typing import Optional
 
-from Yuriko import (
+from Predator import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -13,18 +13,18 @@ from Yuriko import (
     WOLVES,
     dispatcher,
 )
-from Yuriko.modules.helper_funcs.chat_status import (
+from Predator.modules.helper_funcs.chat_status import (
     dev_plus,
     sudo_plus,
     whitelist_plus,
 )
-from Yuriko.modules.helper_funcs.extraction import extract_user
-from Yuriko.modules.log_channel import gloggable
+from Predator.modules.helper_funcs.extraction import extract_user
+from Predator.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.utils.helpers import mention_html
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "Yuriko/elevated_users.json")
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "Predator/elevated_users.json")
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
