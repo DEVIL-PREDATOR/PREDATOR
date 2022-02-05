fdeec976e59e073cbee42881a35796976d4464a3
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-05 20:20:50 +0530

    Update debug.py

--- PREDATOR/modules/debug.py
+++ PREDATOR/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler
 
-from Yuriko import telethn, dispatcher
-from Yuriko.modules.helper_funcs.chat_status import dev_plus
+from Predator import telethn, dispatcher
+from Predator.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
