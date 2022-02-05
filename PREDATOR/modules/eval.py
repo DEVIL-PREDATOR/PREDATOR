f601b117926234ed7f7b7718e24b91de636bf75f
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-05 20:28:37 +0530

    Update eval.py

--- PREDATOR/modules/eval.py
+++ PREDATOR/modules/eval.py
@@ -6,8 +6,8 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from Yuriko import LOGGER, dispatcher
-from Yuriko.modules.helper_funcs.chat_status import dev_plus
+from Predator import LOGGER, dispatcher
+from Predator.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
