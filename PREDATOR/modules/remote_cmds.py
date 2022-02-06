25600c16876983dc7aea6bf1a0d3755a09cf8e78
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-06 21:19:51 +0530

    Update remote_cmds.py

--- PREDATOR/modules/remote_cmds.py
+++ PREDATOR/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from Yuriko import dispatcher, LOGGER
-from Yuriko.modules.helper_funcs.chat_status import (
+from Predator import dispatcher, LOGGER
+from Predator.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from Yuriko.modules.helper_funcs.extraction import extract_user_and_text
-from Yuriko.modules.helper_funcs.filters import CustomFilters
+from Predator.modules.helper_funcs.extraction import extract_user_and_text
+from Predator.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
