8f6e379470151970ac76a43d2e5106da02254e5b
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-06 20:16:48 +0530

    Update math.py

--- PREDATOR/modules/math.py
+++ PREDATOR/modules/math.py
@@ -1,8 +1,8 @@
 import math
 
 import pynewtonmath as newton
-from Yuriko import dispatcher
-from Yuriko.modules.disable import DisableAbleCommandHandler
+from Predator import dispatcher
+from Predator.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
