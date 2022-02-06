c6aa1ffc0370badaf609b081c9eb2557da14d716
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-06 21:35:36 +0530

    Update shell.py

--- PREDATOR/modules/shell.py
+++ PREDATOR/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from Yuriko import LOGGER, dispatcher
-from Yuriko.modules.helper_funcs.chat_status import dev_plus
+from Predator import LOGGER, dispatcher
+from Predator.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
