bc692b9ab305bcf7b24b5fbe89e4be9b5854fadf
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-06 20:12:43 +0530

    Update logomaker.py

--- PREDATOR/modules/logomaker.py
+++ PREDATOR/modules/logomaker.py
@@ -3,8 +3,8 @@ import random
 import glob
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterPhotos
-from Yuriko.events import register
-from Yuriko import telethn as tbot, ubot2
+from Predator.events import register
+from Predator import telethn as tbot, ubot2
 
 
 def mediainfo(media):
