1952cbd1909d06f984474f359688b71c711131b5
Himanshu Mishra <mrrhimanshu98@gmail.com>
2022-02-07 00:30:49 +0530

    Update errors.py

--- PREDATOR/PLUGINSS/errors.py
+++ PREDATOR/PLUGINSS/errors.py
@@ -2,7 +2,7 @@ import sys
 import traceback
 from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-from Yuriko import EVENT_LOGS, pbot as app
+from Predator import EVENT_LOGS, pbot as app
 
 
 def split_limits(text):
